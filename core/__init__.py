ed6323a5117f0b8eaee7ec0351cabd3117bcd687
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-01-23 13:58:54 -0300

    feat: add exceptions

--- core/__init__.py
+++ core/__init__.py
@@ -8,40 +8,19 @@ CLIPBOARD_FILE = Path(__file__).parent.parent.joinpath(".clipboard").resolve()
 ACTION_FILE = Path(__file__).parent.parent.joinpath(".action").resolve()
 
 
-def create_files_if_not_exists() -> None:
-    for file in [CLIPBOARD_FILE, ACTION_FILE]:
-        if not file.is_file():
-            file.touch()
+class Actions:
+    COPY = "copy"
+    MOVE = "move"
+    PASTE = "paste"
 
 
-def add_to_clipboard(action: str, path: PathLike) -> None:
-    create_files_if_not_exists()
-
-    path_str = path if isinstance(path, str) else path.as_posix()
-
-    clipboard_items: list[str] = [
-        item.strip()
-        for item in CLIPBOARD_FILE.read_text(encoding="utf-8").split("\n")
-        if item != ""
-    ]
-    clipboard_items.insert(0, path_str)
-
-    if len(clipboard_items) > MAX_ITEMS_IN_CLIPBOARD:
-        clipboard_items.pop()
-
-    CLIPBOARD_FILE.write_text("\n".join(clipboard_items), encoding="utf-8")
-    ACTION_FILE.write_text(action, encoding="utf-8")
+class InvalidPathError(Exception):
+    pass
 
 
-def get_action() -> str:
-    return ACTION_FILE.read_text(encoding="utf-8").strip()
+class InvalidActionError(Exception):
+    pass
 
 
-def read_from_clipboard() -> Path:
-    clipboard_items: list[str] = [
-        item.strip()
-        for item in CLIPBOARD_FILE.read_text().split("\n")
-        if item != ""
-    ]
-
-    return Path(clipboard_items[0])
+class OutputIsNonDirectoryError(Exception):
+    pass
